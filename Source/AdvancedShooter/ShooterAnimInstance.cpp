06479b4287613d841f87a982f9318b9be44605e6
Felipe Viana <felipetiburcioviana@gmail.com>
2024-04-03 19:41:39 -0700

    Anim Instance

--- Source/AdvancedShooter/ShooterAnimInstance.cpp
+++ Source/AdvancedShooter/ShooterAnimInstance.cpp
@@ -2,4 +2,40 @@
 
 
 #include "ShooterAnimInstance.h"
+#include "SooterCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+void UShooterAnimInstance::UpdateAnimationProperties(float DeltaTime)
+{
+	if (ShooterCharacter == nullptr)
+	{
+		ShooterCharacter = Cast<ASooterCharacter>(TryGetPawnOwner());
+	}
+
+	if (ShooterCharacter)
+	{
+		//Get Speed of the Character from velocity 
+		FVector Velocity{ ShooterCharacter->GetVelocity() };
+		Velocity.Z = 0;
+		Speed = Velocity.Size();
+
+		// Is The Character in the air?
+		bIsInAir = ShooterCharacter->GetCharacterMovement()->IsFalling();
+
+		// Is the Character accelerating 
+
+		if (ShooterCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.f)
+		{
+			bIsAccelerating = true;
+		}
+		else 
+		{
+			bIsAccelerating = false;
+		}
+	}
+}
+
+void UShooterAnimInstance::NativeInitializeAnimation()
+{
+	ShooterCharacter = Cast<ASooterCharacter>(TryGetPawnOwner());
+}
--- Source/AdvancedShooter/ShooterAnimInstance.h
+++ Source/AdvancedShooter/ShooterAnimInstance.h
@@ -9,9 +9,31 @@
 /**
  * 
  */
+class ASooterCharacter;
+
 UCLASS()
 class ADVANCEDSHOOTER_API UShooterAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable)
+	void UpdateAnimationProperties(float DeltaTime);
+
+	virtual void NativeInitializeAnimation() override;
 	
+private:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	ASooterCharacter* ShooterCharacter;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float Speed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	bool bIsInAir;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	bool bIsAccelerating;
 };
